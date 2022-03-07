43d4ca481e729fc1c962e3def0362c79b78bb129
Mirek Simek <miroslav.simek@gmail.com>
2022-03-07 09:24:25 +0100

    cleaning up

--- nr_theses_metadata/constants.py
+++
@@ -1,4 +0,0 @@
-import os
-
-THESES_ALLOWED_SCHEMAS = ['nr_theses_metadata/nr-theses-metadata-v1.0.0.json']
-THESES_PREFERRED_SCHEMA = 'nr_theses_metadata/nr-theses-metadata-v1.0.0.json'
--- nr_theses_metadata/marshmallow/subschemas.py
+++
@@ -1,7 +0,0 @@
-from invenio_records_rest.schemas.fields import SanitizedUnicode
-from marshmallow.fields import List
-
-
-class StudyFieldMixin:
-    AKVO = SanitizedUnicode()
-    aliases = List(SanitizedUnicode())
