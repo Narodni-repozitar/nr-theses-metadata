d03ec933d932601664470b4c6494da8d25d60eff
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-03-10 16:30:16 +0100

    docker build setup

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+
+set -e
+
+source .venv/bin/activate
+
+rm -f dist/*
+rm -f nr-theses-metadata-sample-site/dist/*
+rm -f nr-theses-metadata-sample-app/dist/*
+
+pipenv run pip install wheel
+
+poetry build
+
+cp dist/nr-theses-metadata*.tar.gz nr-theses-metadata-sample-site/dist/
+
+cd nr-theses-metadata-sample-app
+
+pipenv run python3 setup.py sdist bdist_wheel
+
+cp dist/nr-theses-metadata-sample-app*.tar.gz ../nr-theses-metadata-sample-site/dist/
+
+cd ../nr-theses-metadata-sample-site
+
+# Clear pipenv cache to avoid hash issues
+pipenv --clear
+pipenv lock
+invenio-cli containers build
